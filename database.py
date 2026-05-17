ca02eb3cfee73303903cb23a5f137383f3502144
Mansour Ayman <mansourayman1235@gmail.com>
2026-05-17 23:18:00 +0300

    fix railway backend

--- database.py
+++ database.py
@@ -1,6 +1,26 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base;
-url="mysql+mysqlconnector://root@127.0.0.1:3306/health_monitoring"
-engine=create_engine(url,echo=True)
-sessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False);
-Base=declarative_base();
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./health_monitoring.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    echo=True
+)
+
+sessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from fastapi import FastAPI
-from .database import Base,engine;
-from .routers.auth import router as auth_router
-from .routers.heart_rate import router as heart_router;
-from .routers.device import router as device_router
-from .routers.activity import router as activity_router
-from .routers.ai_activity import router as ai_activity_router
-from .routers.alerts import router as alert_router
-from .routers.device import router as device;
+
+from database import Base, engine
+
+from routers import auth
+from routers import device
+from routers import heart_rate
+from routers import activity
+from routers import ai_activity
+from routers import alerts
+from routers import user_activity
 
 Base.metadata.create_all(bind=engine)
-app=FastAPI()
-app.include_router(auth_router)
-app.include_router(ai_activity_router)
-app.include_router(heart_router)
-app.include_router(device_router)
-app.include_router(activity_router);
-app.include_router(alert_router)    
-app.include_router(device)    
+
+app = FastAPI(
+    title="Healthcare Backend",
+    version="1.0.0"
+)
+
+app.include_router(auth.router)
+app.include_router(device.router)
+app.include_router(heart_rate.router)
+app.include_router(activity.router)
+app.include_router(ai_activity.router)
+app.include_router(alerts.router)
+app.include_router(user_activity.router)
+
 
 @app.get("/")
-async def root():
-    return {"message":"healthmonitor"}
+def root():
+    return {
+        "message": "Healthcare Backend Running"
+    }
