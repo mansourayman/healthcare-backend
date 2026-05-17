1a6cf591ca770e008e6147aef0ab74a49593b178
Mansour Ayman <mansourayman1235@gmail.com>
2026-05-18 00:23:17 +0300

    fix bcrypt password some

--- security/passwordFunc.py
+++ security/passwordFunc.py
@@ -1,20 +1,14 @@
 from passlib.context import CryptContext
 
-pwx = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
-
+pwx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def encryptPassword(password: str):
     password = password[:72]
     return pwx.hash(password)
 
-
 def checkPassword(plain_password: str, hashed_password: str):
     plain_password = plain_password[:72]
     return pwx.verify(plain_password, hashed_password)
-
 
 def verifyPassword(plain_password: str, hashed_password: str):
     return checkPassword(plain_password, hashed_password)
