01bb14ff0d7ca246a0cfb70c58d2fa03baaa1331
Mansour Ayman <mansourayman1235@gmail.com>
2026-05-17 23:51:08 +0300

    fix bcrypt password limit

--- security/passwordFunc.py
+++ security/passwordFunc.py
@@ -1,6 +1,16 @@
-from passlib.context import CryptContext;
-pwx=CryptContext(schemes=["bcrypt"], deprecated="auto");
-def encryptPassword(password:str)->str:
-    return pwx.hash(password);
-def checkPassword(password:str,hashedPassword:str)->bool:
-    return pwx.verify(password,hashedPassword);
+from passlib.context import CryptContext
+
+pwx = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def encryptPassword(password: str):
+    password = password[:72]
+    return pwx.hash(password)
+
+
+def verifyPassword(plain_password: str, hashed_password: str):
+    plain_password = plain_password[:72]
+    return pwx.verify(plain_password, hashed_password)
